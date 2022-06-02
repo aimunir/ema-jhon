c146dab598b06af15c84042cec38d2ae7bb8c27a
aimunir <aimunir25@outlook.com>
2022-06-02 17:47:43 +0600

    seller,shipping,stock add

--- src/Compotents/Product/Product.js
+++ src/Compotents/Product/Product.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import './Product.css'
 
 
 const Product = (props) => {
     console.log(props.product.name);
+    const {img,name,seller,price,stock,shipping}=props.product;
   return (
-    <div>
-        <h4>{props.product.name}</h4>
+    <div className='products'>
 
+        <div> 
+            <img src={img} alt="" />
+        </div>
+
+        <div className='product-name'>
+        <h5 id='name'>{name}</h5><br />
+        <p>by:{seller}</p> <p>Price : ${price}</p>
+        <p>Shipping : $/{shipping}</p>
+        <p>Only {stock} left . Order Now</p>      
+        </div>      
     </div>
   )
 }
