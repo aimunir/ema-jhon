f1bdacf3a4d856eabc3ed831d07ddc47b2c72d71
aimunir <aimunir25@outlook.com>
2022-06-02 15:20:35 +0600

    flex added to shop container

--- src/Compotents/Shop/Shop.js
+++ src/Compotents/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import fakeData from '../../fakeData'
+import './Shop.css'
 const Shop = () => {
     
 const first10 = fakeData.slice(0,12);
@@ -9,7 +10,7 @@ const [products,setproducts]=useState(first10);
 
     
   return (
-    <div className=' ' > 
+    <div className='shop-container'> 
 
       <div className="product-container">
       <h3>{products.length}</h3>
